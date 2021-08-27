3cb3c3bb23ab0438ea614ac7205996a550176271
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2021-08-27 10:40:25 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,19 +1,29 @@
+  // Instantiate Config Object
   var config = {
    type: Phaser.AUTO,
    width: 800,
    height: 600,
    scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
    }
   };
 
+  // Instantiate Game Object
   var game = new Phaser.Game(config);
 
+  // Preload
   function preload() {
 
   }
 
+  // Create
   function create() {
+
+  }
+
+  // Update
+  function update() {
 
   }
